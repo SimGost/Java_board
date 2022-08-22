a563e8b64959220441255e4e2e9a506b1e67a479
_Gost_ <kaplan.lera@gmail.com>
2022-08-22 19:54:30 +0300

    Java_board

--- Board.java
+++ Board.java
@@ -1,20 +1,21 @@
 
 public class Board {
     private int[][] board;
+    // creating a board size variables 
     int k;
     int c;
-    public void setsize(int a,int d){
-        k = a;
-        c = d;
-    } 
+    
+    
     
 
     
     
 
     public Board() {
-        // create a new board
-        setsize(5, 7);
+        // creating a new board
+        k = 3;
+        c = 3;
+
         this.board = new int[k][c];
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < c; j++) {
@@ -22,6 +23,19 @@ public class Board {
             }
         }
     }
+
+    public Board(int a, int d){
+        k = a;
+        c = d;
+
+        this.board = new int[k][c];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < c; j++) {
+                this.board[i][j] = 0;
+            }
+        }
+    }
+
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- BoardTest.java
+++ BoardTest.java
@@ -1,3 +1,8 @@
 public class BoardTest {
-
+    public static void main(String[] args) {
+        Board board = new Board(); // create a new board
+        System.out.println(board);
+        Board board1 = new Board(5,5); // create a second new board
+        System.out.println(board1);
+    }
 }
